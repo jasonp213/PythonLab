8637b205781e56b68e0318bd8f8a03ee61a55afe
Jason Peng <mijasoda@gmail.com>
2021-11-23 11:08:59 +0800

    update: makefile using same env brackets

--- elasticsearch-dsl-example/src/persistence.py
+++ elasticsearch-dsl-example/src/persistence.py
@@ -1,29 +1,15 @@
-from datetime import datetime
-from elasticsearch_dsl import Document, InnerDoc, Integer, Text, Keyword, GeoPoint, Date
+"""
+Here define the Elasticsearch(kibana) document
+
+https://www.elastic.co/guide/en/kibana/7.6/tutorial-build-dashboard.html#load-dataset
+
+"""
+
+from elasticsearch_dsl import Document, Integer, Text, Keyword, GeoPoint, Date
 
 
 class Shakespeare(Document):
-    """
-    {
-        "line_id": INT,
-        "play_name": "String",
-        "speech_number": INT,
-        "line_number": "String",
-        "speaker": "String",
-        "text_entry": "String",
-    }
-    {
-      "mappings": {
-        "properties": {
-            "speaker": {"type": "keyword"},
-            "play_name": {"type": "keyword"},
-            "line_id": {"type": "integer"},
-            "speech_number": {"type": "integer"}
-        }
-      }
-    }
 
-    """
     line_id = Integer()
     play_name = Keyword()
     speech_number = Integer()
@@ -33,54 +19,37 @@ class Shakespeare(Document):
 
 
 class Accounts(Document):
-    """
-    {
-        "account_number": INT,
-        "balance": INT,
-        "firstname": "String",
-        "lastname": "String",
-        "age": INT,
-        "gender": "M or F",
-        "address": "String",
-        "employer": "String",
-        "email": "String",
-        "city": "String",
-        "state": "String"
-    }
-    """
+
     account_number = Integer()
     balance = Integer()
     firstname = Text()
     lastname = Text()
     age = Integer()
-    gender = "M or F",
+    gender = Keyword()  # "M or F"
     address = Text()
     employer = Text()
     email = Text()
     city = Text()
     state = Text()
 
-class LogStash(Document):
-    """
-    {
-        "memory": INT,
-        "geo.coordinates": "geo_point"
-        "@timestamp": "date"
-    }
-    {
-      "mappings": {
-        "properties": {
-          "geo": {
-            "properties": {
-              "coordinates": {
-                "type": "geo_point"
-              }
-            }
-          }
+    class Index:
+        name = 'bank'
+
+        settings = {
+            "number_of_shards": 1,
+            # Note that the number of replicas 0 means
+            "number_of_replicas": 0
         }
-      }
-    }
-    """
+
+
+class LogStash(Document):
+
     memory = Integer()
     geo_coordinates = GeoPoint()
     timestamp = Date()
+
+    class Index:
+        """
+        this define the index meta
+        """
+        name = 'logstash'
