6a3b2d3541ab85eadb5f4b1e2743386191351787
Peng James <james@labsgl.com>
2021-11-25 18:04:55 +0800

    update es persistence

--- elasticsearch-dsl-example/src/persistence_example.py
+++ elasticsearch-dsl-example/src/persistence_example.py
@@ -1,9 +1,8 @@
 from datetime import datetime
+
 from elasticsearch_dsl import Document, Date, Integer, Keyword, Text
 from elasticsearch_dsl.connections import connections
 
-# Define a default Elasticsearch client
-connections.create_connection(hosts=['localhost'])
 
 class Article(Document):
     title = Text(analyzer='snowball', fields={'raw': Keyword()})
@@ -15,27 +14,32 @@ class Article(Document):
     class Index:
         name = 'blog'
         settings = {
-          "number_of_shards": 2,
+            "number_of_shards": 2,
         }
 
-    def save(self, ** kwargs):
+    def save(self, **kwargs):
         self.lines = len(self.body.split())
         return super(Article, self).save(**kwargs)
 
     def is_published(self):
         return datetime.now() >= self.published_from
 
-# create the mappings in elasticsearch
-Article.init()
 
-# create and save and article
-article = Article(meta={'id': 43}, title='Hello world!', tags=['test'])
-article.body = ''' looong text '''
-article.published_from = datetime.now()
-article.save()
+if __name__ == '__main__':
+    # Define a default Elasticsearch client
+    connections.create_connection(hosts=['localhost'])
 
-article = Article.get(id=42)
-print(article.is_published())
+    # create the mappings in elasticsearch
+    Article.init()
 
-# Display cluster health
-print(connections.get_connection().cluster.health())
+    # create and save and article
+    article = Article(meta={'id': 43}, title='Hello world!', tags=['test'])
+    article.body = ''' looong text '''
+    article.published_from = datetime.now()
+    article.save()
+
+    article = Article.get(id=42)
+    print(article.is_published())
+
+    # Display cluster health
+    print(connections.get_connection().cluster.health())
