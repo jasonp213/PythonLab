a9b992943ba0b71b6d70c7f86c57eb2c1b9104df
Jason Peng <mijasoda@gmail.com>
2021-11-25 18:04:55 +0800

    update es persistence

--- elasticsearch-dsl-example/src/persistence.py
+++ elasticsearch-dsl-example/src/persistence.py
@@ -5,11 +5,11 @@ https://www.elastic.co/guide/en/kibana/7.6/tutorial-build-dashboard.html#load-da
 
 """
 
-from elasticsearch_dsl import Document, Integer, Text, Keyword, GeoPoint, Date, InnerDoc
+from elasticsearch_dsl import Document, Integer, Text, Keyword, GeoPoint, Date
+from elasticsearch_dsl.connections import connections
 
 
 class Shakespeare(Document):
-
     line_id = Integer()
     play_name = Keyword()
     speech_number = Integer()
@@ -19,7 +19,6 @@ class Shakespeare(Document):
 
 
 class Bank(Document):
-
     account_number = Integer()
     balance = Integer()
     firstname = Text()
@@ -42,7 +41,6 @@ class Bank(Document):
 
 
 class LogStash(Document):
-
     memory = Integer()
     geo_coordinates = GeoPoint()
     timestamp = Date()
@@ -53,7 +51,30 @@ class LogStash(Document):
         """
         name = 'logstash-*'
         aliases = {
-            'logstash': {}
+            'logstash': {
+                # here could put filter
+            }
         }
 
 
+def setup_log():
+    alias = 'logstash'
+    patten = 'logstash-*'  # using wildcard char
+
+    template = LogStash._index.as_template(alias, patten)
+
+    return template.save()
+
+
+if __name__ == '__main__':
+    # python shell template script
+    # import os
+    #
+    # sys.path.append(f'{os.getcwd()}/elasticsearch-dsl-example/src')
+    # from persistence import *
+    # from
+    # from elasticsearch_dsl.connections import connections
+
+    connections.create_connection(hosts=['localhost'])
+
+    print(setup_log())
