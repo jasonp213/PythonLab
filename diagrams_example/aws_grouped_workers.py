b33951d29b39cdcdc1118247708e1da825853281
Peng James <james@labsgl.com>
2022-02-15 10:14:03 +0800

    add python diagrams

---
+++ diagrams_example/aws_grouped_workers.py
@@ -0,0 +1,30 @@
+from diagrams import Diagram, Cluster
+from diagrams.aws.compute import EC2, ECS
+from diagrams.aws.database import RDS, ElastiCache
+from diagrams.aws.network import ELB, Route53
+
+with Diagram("Grouped Workers", show=False, direction="TB"):
+    ELB("lb") >> [EC2("worker1"),
+                  EC2("worker2"),
+                  EC2("worker3"),
+                  EC2("worker4"),
+                  EC2("worker5")] >> RDS("events")
+
+with Diagram("Clustered Web Services", show=False):
+    dns = Route53("dns")
+    lb = ELB("lb")
+
+    with Cluster("Services"):
+        svc_group = [ECS("web1"),
+                     ECS("web2"),
+                     ECS("web3")]
+
+    with Cluster("DB Cluster"):
+        db_primary = RDS("userdb")
+        db_primary - [RDS("userdb ro")]
+
+    memcached = ElastiCache("memcached")
+
+    dns >> lb >> svc_group
+    svc_group >> db_primary
+    svc_group >> memcached
